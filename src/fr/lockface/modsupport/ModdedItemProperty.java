41fc9d382ba471dde3c54529aa18e781418c4726
Nicolas Went <nicolas.went@epita.fr>
2020-04-20 21:13:03 +0200

    Version with Optional for Material

--- src/fr/lockface/modsupport/ModdedItemProperty.java
+++ src/fr/lockface/modsupport/ModdedItemProperty.java
@@ -1,5 +1,7 @@
 package fr.lockface.modsupport;
 
+import java.util.Optional;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -7,7 +9,7 @@ import org.bukkit.inventory.ItemStack;
 import me.hsgamer.bettergui.object.Icon;
 import me.hsgamer.bettergui.object.property.item.ItemProperty;
 
-public class ModdedItemProperty extends ItemProperty<String, Material>
+public class ModdedItemProperty extends ItemProperty<String, Optional<Material>>
 {
 
 	public ModdedItemProperty(Icon icon)
@@ -18,29 +20,30 @@ public class ModdedItemProperty extends ItemProperty<String, Material>
 	@Override
 	public boolean compareWithItemStack(Player p, ItemStack toCompare) 
 	{
-		Material parsed = getParsed(p);
+		Optional<Material> parsed = getParsed(p);
 		Material compare = toCompare.getType();
-		return compare.equals(parsed);
+		return parsed.isPresent() && compare.equals(parsed.get());
 	}
 
 	@Override
-	public Material getParsed(Player p)
+	public Optional<Material> getParsed(Player p)
 	{
 		Material ret = Material.valueOf(getValue());
 		if (ret == null)
 		{
 			System.out.println("[BetterGUI][Modded-Support addon] Error, " + getValue() +
 				" is not valid, use /mohist item info to get the valid type");
-			return Material.valueOf("STONE");
+			return Optional.empty();
 		}
-		return ret;
+		return Optional.of(ret);
 	}
 
 	@Override
 	public ItemStack parse(Player p, ItemStack parent)
 	{
-		Material parsed = getParsed(p);
-		parent.setType(parsed);
+		Optional<Material> parsed = getParsed(p);
+		if (parsed.isPresent())
+			parent.setType(parsed.get());
 		return parent;
 	}
 	
